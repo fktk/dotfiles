a10407e5d39a2f8f53407dcdae3c211b26eaf8bb
Takefumi Fukumoto <windwild12@gmail.com>
2026-04-10 23:36:10 +0900

    Add .bashrc, tmux.conf and update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # ~/.dotfilesにファイルを置いた場合
 DOTFILES_DIR=$HOME/.dotfiles
 
-ln -sf $DOTFILES_DIR/nvim $HOME/.config/nvim
-ln -sf $DOTFILES_DIR/tmux $HOME/.config/tmux
-ln -sf $DOTFILES_DIR/yazi $HOME/.config/yazi
+ln -sf $DOTFILES_DIR/nvim $HOME/.config
+ln -sf $DOTFILES_DIR/tmux $HOME/.config
+ln -sf $DOTFILES_DIR/yazi $HOME/.config
+ln -sf $DOTFILES_DIR/.bashrc $HOME/.bashrc
